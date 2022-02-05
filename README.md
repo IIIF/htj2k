1bd35b56699b95041c50cdeffd1dfc506b120d3e
Stefano Cossu <stefano@cossu.cc>
2022-02-04 16:05:31 -0800

    Replace ptiff shell script with Python script; adjust documentation.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # htj2k
-A project to evaluate htj2k for IIIF
+
+A project to evaluate htj2k for IIIF.
+
+Follow setup instructions below and then specific instructions for running the
+conversion and benchmarks in the individual subfolders.
+
+## Setup Instructions (*nix)
+
+Install [LibVIPS](https://www.libvips.org/install.html) and
+[Kakadu](https://kakadusoftware.com/documentation-downloads/downloads/) in
+your operating system.
+
+Create a project folder:
+
+```
+mkdir htj2k
+```
+
+Create Python virtual environment:
+
+```
+cd htj2k
+python3 -m venv venv
+source venv/bin/activate
+```
+
+Clone this repo:
+
+```
+git clone git@github.com:IIIF/htj2k.git src
+```
+
+Install dependencies:
+
+```
+cd src
+pip install -r requirements.txt
+```
--- convert/convert.sh
+++ convert/convert.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ ( -z "${1}" ) || ( -z "${2}" ) || ( "${3}" != "ptiff" && "${3}" != "jp2" && "${3}" != "jp2_legacy" ) ]]; then
-    echo "Usage: ${0} <source> <base output path> <output format>\n"\
+if [[ ( -z "${1}" ) || ( -z "${2}" ) || ( "${3}" != "ptiff" && "${3}" != "jp2" ) ]]; then
+    echo -e "Usage: ${0} <source> <base output path> <output format>\n"\
         "Where <source> is the full "\
         "path to the source file(s) (glob patterns and directories are "\
-        "allowed, enclosed in quotes), <base output path> is the path under "\
+        "allowed), <base output path> is the path under "\
         "which per-format output folders will be created and <output format> "\
-        "is one of 'ptiff', 'jp2' or 'jp2_legacy'."
+        "is one of 'ptiff' or 'jp2'."
     exit 1
 fi
 
@@ -22,13 +22,10 @@ for in_path in $in_ptn; do
     echo "Converting $in_fname to $format format..."
 
     if [ "${format}" = "ptiff" ]; then
-        out_path="${out_base_path}/ptiff/${in_fname%.*}.tif"
-        cmd="./tiff_to_pyramid.sh /tmp ${in_path} ${out_path}"
-    elif [ "${format}" = "jp2_legacy" ]; then
-        out_path="${out_base_path}/jp2_legacy/${in_fname%.*}.jp2"
-        cmd="./tiff_to_jp2_legacy.sh ${in_path} ${out_path}"
+        out_path="${out_base_path}/${in_fname%.*}.tif"
+        cmd="./imgconv_ptiff.py ${in_path} ${out_path}"
     else
-        out_path="${out_base_path}/jp2/${in_fname%.*}.jp2"
+        out_path="${out_base_path}/${in_fname%.*}.jp2"
         cmd="./tiff_to_jp2.sh ${in_path} ${out_path}"
     fi
 
