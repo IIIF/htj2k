8b7aed09e9082bc2bd406b2529822e845fcb89b1
Stefano Cossu <stefano@cossu.cc>
2022-03-29 10:39:39 -0700

    Add initial iipsrv container from Getty (no JP2 encoder)

---
+++ image_server/iipsrv_entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export BASE_URL="${GCISWG_PUBLIC_EP}/iiif/image/"
+export CACHE_CONTROL="${IIPSRV_CACHE_CONTROL}"
+export FILESYSTEM_PREFIX="${IIPSRV_FILESYSTEM_PREFIX}"
+export HTTP_PORT="${IIPSRV_HTTP_PORT}"
+export INTERPOLATION="${IIPSRV_INTERPOLATION}"
+export JPEG_QUALITY="${IIPSRV_JPEG_QUALITY}"
+export MAX_CVT="${IIPSRV_MAX_CVT}"
+export MAX_IMAGE_CACHE_SIZE="${IIPSRV_MAX_IMAGE_CACHE_SIZE}"
+export VERBOSITY="${IIPSRV_VERBOSITY}"
+
+if [ "${GCISWG_APP_MODE}" = "development" ]; then
+    exec /var/www/iipsrv/cgi-bin/iipsrv.fcgi --bind 0.0.0.0:${IIPSRV_CGI_PORT}
+else
+    exec /usr/bin/supervisord -c /etc/supervisord.conf
+fi
