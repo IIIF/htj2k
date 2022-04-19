c462d520ec5b692bb596f33b0f249bc0e260ac3c
michaeldsmith <37905569+michaeldsmith@users.noreply.github.com>
2022-04-19 13:01:35 -0700

    add scripts for bulk testing

--- README.md
+++ README.md
@@ -37,3 +37,15 @@ Install dependencies:
 cd src
 pip install -r requirements.txt
 ```
+
+# docker
+## build docker
+`docker build --rm -f ./src/Dockerfile -t iiif-htj2k:latest .`
+## run docker
+`docker run -it --rm  iiif-htj2k:latest`
+`docker run -it --rm -v /home/miksmith/2022_04_18/:/usr/src/iiif-htj2k/build/ iiif-htj2k:latest`
+
+## run compression script on TIF files within the original/GRI directory
+`bash ./src/convert/convert.sh ./build/original/GRI/ ./build/compressed/ ptiff ./build/logs/`
+`bash ./src/convert/convert.sh ./build/test/ ./build/compressed/ ptiff ./build/logs`
+`bash ./src/scripts/run_all_encodes.sh`
--- convert/convert.sh
+++ convert/convert.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [[ ( -z "${1}" ) || ( -z "${2}" ) || ( "${3}" != "ptiff" && "${3}" != "jp2" && "${3}" != "htj2k") ]]; then
-    echo -e "Usage: ${0} <source> <base output path> <output format>\n"\
+set -u
+#set -e
+set -x
+
+if [[ ( -z "${1}" ) || ( -z "${2}" ) || ( "${3}" != "ptiff_lossy" && "${3}" != "ptiff_lossless" && "${3}" != "j2k1_lossy" && "${3}" != "j2k1_lossless" && "${3}" != "htj2k_lossy" && "${3}" != "htj2k_lossless") ]]; then
+    echo -e "Usage: ${0} <source> <base output path> <output format> <logs_directory>\n"\
         "Where <source> is the full "\
         "path to the source file(s) (glob patterns and directories are "\
         "allowed), <base output path> is the path under "\
         "which per-format output folders will be created and <output format> "\
-        "is one of 'ptiff' or 'jp2' or 'htj2k'."
+        "is one of 'ptiff_lossy', 'ptiff_lossless' 'j2k1_lossy', 'j2k1_lossless', 'htj2k_lossy' or 'htj2k_lossless'."
     exit 1
 fi
 
@@ -15,27 +19,82 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 in_ptn=$1
 out_base_path=$2
 format=$3
+logs_directory=$4
 echo "Input pattern: ${in_ptn}"
 
-for in_path in $in_ptn; do
+number_of_encode_cycle_iterations=5
+
+# setup log files
+date_for_filename=`date +"%Y-%m-%d-%H-%M-%S_%N"`
+log_filename="$logs_directory/$format.${date_for_filename}.log.txt" 
+
+# write header to log file
+printf "in_ptn,format,encode_time_seconds,bytes_in_compressed_folder\n" > $log_filename
+
+# start timer
+start_job_time=`date +%s%N`
+
+for in_path in $in_ptn/*.tif; do
     echo "input image: ${in_path}"
     in_fname=$(basename $in_path)
 
     echo "Converting $in_fname to $format format..."
 
-    if [ "${format}" = "ptiff" ]; then
-        out_path="${out_base_path}/${in_fname%.*}.tif"
-        cmd="$SCRIPT_DIR/tiff_to_ptiff.sh ${in_path} ${out_path}"
-    elif [ "${format}" = "jp2" ]; then
-        out_path="${out_base_path}/${in_fname%.*}.jp2"
-        cmd="$SCRIPT_DIR/tiff_to_jp2.sh ${in_path} ${out_path}"
-    elif [ "${format}" = "htj2k" ]; then
-        out_path="${out_base_path}/${in_fname%.*}.htj2k.jp2"
-        cmd="$SCRIPT_DIR/tiff_to_htj2k.sh ${in_path} ${out_path}"
-    fi
+    for encoding_cycle_iteration in $(seq 1 $number_of_encode_cycle_iterations)
+    do
+        if [ "${format}" = "ptiff_lossy" ]; then
+            out_path="${out_base_path}/${in_fname%.*}.tif"
+            cmd="$SCRIPT_DIR/tiff_to_ptiff_lossy_jpeg_Q90.sh ${in_path} ${out_path}"
+
+        elif [ "${format}" = "ptiff_lossless" ]; then
+            out_path="${out_base_path}/${in_fname%.*}.tif"
+            cmd="$SCRIPT_DIR/tiff_to_ptiff_lossless.sh  ${in_path} ${out_path}"
+            
+        elif [ "${format}" = "j2k1_lossy" ]; then
+            out_path="${out_base_path}/${in_fname%.*}.jp2"
+            cmd="$SCRIPT_DIR/tiff_to_jp2_lossy_rate_3bpp.sh ${in_path} ${out_path}"
+        
+        elif [ "${format}" = "j2k1_lossless" ]; then
+            out_path="${out_base_path}/${in_fname%.*}.jp2"
+            cmd="$SCRIPT_DIR/tiff_to_jp2_lossless.sh ${in_path} ${out_path}"
+
+        elif [ "${format}" = "htj2k_lossy" ]; then
+            out_path="${out_base_path}/${in_fname%.*}.htj2k.jp2"
+            cmd="$SCRIPT_DIR/tiff_to_htj2k_lossy_rate_3bpp.sh ${in_path} ${out_path}"
+
+        elif [ "${format}" = "htj2k_lossless" ]; then
+            out_path="${out_base_path}/${in_fname%.*}.htj2k.jp2"
+            cmd="$SCRIPT_DIR/tiff_to_htj2k_lossless.sh ${in_path} ${out_path}"
+
+        fi
+
+    done
 
     $cmd
 
     echo "...Done."
     echo
 done
+
+end_job_time=`date +%s%N`
+  
+# compute timing stats for the sequence
+let duration_job_nanoseconds=$end_job_time-$start_job_time
+number_of_nanoseconds_in_one_second=1000000000
+duration_job_total_seconds=$(echo "scale=5;$duration_job_nanoseconds/$number_of_nanoseconds_in_one_second" | bc)
+encode_time_per_iteration_seconds=$(echo "scale=5;($duration_job_nanoseconds/$number_of_encode_cycle_iterations)/$number_of_nanoseconds_in_one_second" | bc)
+  
+# get number of bytes for the folder of compressed files
+executing_directory=$(pwd)
+cd $out_base_path
+bytes_in_compressed_folder_with_dot_dir=`du -sb | awk '{print $1}'`
+# subtract 4096 for the '.' dir
+let bytes_in_compressed_folder=$bytes_in_compressed_folder_with_dot_dir-4096 
+cd $executing_directory
+  
+# print results to screen
+printf "%s %s %s seconds %s bytes\n" $in_ptn ${format} $encode_time_per_iteration_seconds $bytes_in_compressed_folder
+
+# add timing info to log
+
+printf "%s,%s,%s,%s\n" $in_ptn ${format} $encode_time_per_iteration_seconds $bytes_in_compressed_folder >> $log_filename
