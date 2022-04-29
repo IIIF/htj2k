67dfc36b5a541d4256a8621771428771d89b96c5
Stefano Cossu <stefano@cossu.cc>
2022-04-29 12:07:26 -0700

    Single-process Nginx server:
    * Add Nginx to iipsrv
    * Remove supervisord
    * Compact iipsrv configuration (hardcode values)

--- image_server/iipsrv_entrypoint.sh
+++ image_server/iipsrv_entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 
-export BASE_URL="${PUBLIC_EP}/iiif/image/"
-export CACHE_CONTROL="${IIPSRV_CACHE_CONTROL}"
-export FILESYSTEM_PREFIX="${IIPSRV_FILESYSTEM_PREFIX}"
-export HTTP_PORT="${IIPSRV_HTTP_PORT}"
-export INTERPOLATION="${IIPSRV_INTERPOLATION}"
-export JPEG_QUALITY="${IIPSRV_JPEG_QUALITY}"
-export MAX_CVT="${IIPSRV_MAX_CVT}"
-export MAX_IMAGE_CACHE_SIZE="${IIPSRV_MAX_IMAGE_CACHE_SIZE}"
-export VERBOSITY="${IIPSRV_VERBOSITY}"
+set -e
 
-if [ "${APP_MODE}" = "development" ]; then
-    exec /var/www/iipsrv/cgi-bin/iipsrv.fcgi --bind 0.0.0.0:${IIPSRV_CGI_PORT}
-else
-    exec /usr/bin/supervisord -c /etc/supervisord.conf
-fi
+nohup /var/www/iipsrv/cgi-bin/iipsrv.fcgi --bind 0.0.0.0:9000 &
+echo "Starting Nginx."
+exec /usr/sbin/nginx -g 'daemon off;'
