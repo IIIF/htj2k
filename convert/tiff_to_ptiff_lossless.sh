65c997f0dc8e5d49d5185d365dec0f7e01f3eb32
Ruven <ruven@users.sourceforge.net>
2022-05-12 11:24:41 +0200

    Removed unnecessary bigtiff parameter

--- convert/tiff_to_ptiff_lossless.sh
+++ convert/tiff_to_ptiff_lossless.sh
@@ -5,4 +5,4 @@
 
 # you can pass in a glob parameter and a directory as a output filename to process multiple files
 
-vips tiffsave $1 $2 --vips-progress --compression deflate --bigtiff --tile --pyramid --tile-width 256 --tile-height 256 
+vips tiffsave $1 $2 --compression deflate --tile --pyramid --tile-width 256 --tile-height 256 
