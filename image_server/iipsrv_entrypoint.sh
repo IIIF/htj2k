595d22ff29e5c6789e2332ae871fb0bd91b0b9ba
Stefano Cossu <stefano@cossu.cc>
2022-05-17 11:09:46 -0700

    Move ignored Kakadu source; build for both OpenJPEG and Kakadu.

--- image_server/iipsrv_entrypoint.sh
+++ image_server/iipsrv_entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-nohup /var/www/iipsrv/cgi-bin/iipsrv.fcgi --bind 0.0.0.0:9000 &
+echo "Starting iipsrv with OpenJPEG support."
+nohup /var/www/iipsrv/cgi-bin/iipsrv_openjpeg.fcgi --bind 0.0.0.0:9000 &
+echo "Starting iipsrv with Kakadu support."
+nohup /var/www/iipsrv/cgi-bin/iipsrv_kakadu.fcgi --bind 0.0.0.0:9001 &
 echo "Starting Nginx."
 exec /usr/sbin/nginx -g 'daemon off;'
