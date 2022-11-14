3beb9522b0806ad767aac93c836727b0a7243806
Stefano Cossu <stefano_cossu@harvard.edu>
2022-11-14 16:49:52 -0500

    Fix load test batch script.

--- load_test/batch_run.sh
+++ load_test/batch_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export EP=http://localhost:8000
+export EP=http://iipsrv_kdu
 export LOCUSTFILE=load_test/locustfile.py
 
 LOCUST_IMG_DATASET=htj2k_lossless LOCUST_IMG_FORMAT=htj2k.jp2 locust --headless -t 10m -u 1 --csv kdu_htj2k_lossless --print-stats -f $LOCUSTFILE -H $EP
@@ -11,6 +11,6 @@ LOCUST_IMG_DATASET=ptiff_lossless LOCUST_IMG_FORMAT=tif locust --headless -t 10m
 
 LOCUST_IMG_DATASET=ptiff_lossy LOCUST_IMG_FORMAT=tif locust --headless -t 10m -u 1 --csv ptiff_lossy --print-stats -f $LOCUSTFILE -H $EP
 
-LOCUST_IMG_DATASET=j2k1_lossy LOCUST_IMG_FORMAT=jp2 locust --headless -t 10m -u 1 --csv jp2_lossless --print-stats -f $LOCUSTFILE -H $EP
+LOCUST_IMG_DATASET=j2k1_lossless LOCUST_IMG_FORMAT=jp2 locust --headless -t 10m -u 1 --csv j2k1_lossless --print-stats -f $LOCUSTFILE -H $EP
 
-LOCUST_IMG_DATASET=j2k1_lossless LOCUST_IMG_FORMAT=jp2 locust --headless -t 10m -u 1 --csv jp2_lossy --print-stats -f $LOCUSTFILE -H $EP
+LOCUST_IMG_DATASET=j2k1_lossy LOCUST_IMG_FORMAT=jp2 locust --headless -t 10m -u 1 --csv j2k1_lossy --print-stats -f $LOCUSTFILE -H $EP
