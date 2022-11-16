72cdb29d4df1abfb1aba752678a4f024cd7bf794
Stefano Cossu <stefano_cossu@harvard.edu>
2022-11-16 11:29:09 -0500

    Fix Locust test command for HTJ2K lossy.

--- load_test/batch_run.sh
+++ load_test/batch_run.sh
@@ -7,7 +7,7 @@ export LOCUSTFILE=$cwd/locustfile.py
 
 LOCUST_IMG_DATASET=htj2k_lossless LOCUST_IMG_FORMAT=htj2k.jp2 locust --headless -t 10m -u 1 --csv ${cwd}/kdu_htj2k_lossless -f $LOCUSTFILE -H $EP
 
-LOCUST_IMG_DATASET=htj2k_lossy LOCUST_IMG_FORMAT=htj2k.jp2 locust --headless -t 10m -u 1 --csv ${cwd}/kdu_htj2k_lossy -f $LOCUSTFILE/locustfile.py -H $EP
+LOCUST_IMG_DATASET=htj2k_lossy LOCUST_IMG_FORMAT=htj2k.jp2 locust --headless -t 10m -u 1 --csv ${cwd}/kdu_htj2k_lossy -f $LOCUSTFILE -H $EP
 
 LOCUST_IMG_DATASET=ptiff_lossless LOCUST_IMG_FORMAT=tif locust --headless -t 10m -u 1 --csv ${cwd}/ptiff_lossless -f $LOCUSTFILE -H $EP
 
