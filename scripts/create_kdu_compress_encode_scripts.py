b2217e49244543832c3345a4daa5cf7bde9261de
Glen Robson <glen.robson@gmail.com>
2023-05-23 18:16:41 +0100

    Code fixes to make parameters work and other fixes

--- scripts/create_kdu_compress_encode_scripts.py
+++ scripts/create_kdu_compress_encode_scripts.py
@@ -16,77 +16,42 @@ smallest_thumbnail_dimension_default = 90
 
 parser = argparse.ArgumentParser(description="creates bash script to convert TIF files into JPEG2000 files using kdu_compress commands")
 parser.add_argument('--source_directory',
-                      help="directory where the input TIF files are located. default is --source_directory \"%s\"" % (source_directory_default) ) 
+                      help="directory where the input TIF files are located. default is --source_directory \"%s\"" % (source_directory_default), default=source_directory_default) 
 parser.add_argument("--compressed_directory", 
-                      help="directory where output files will be written. default is --compressed_directory \"%s\"" % (compressed_directory_default) )
+                      help="directory where output files will be written. default is --compressed_directory \"%s\"" % (compressed_directory_default), default=source_directory_default )
 parser.add_argument("--logs_directory", 
-                      help="directory where log files will be written. default is --logs_directory \"%s\"" % (logs_directory_default) )
+                      help="directory where log files will be written. default is --logs_directory \"%s\"" % (logs_directory_default), default=logs_directory_default )
 parser.add_argument('--output_script_filename_prefix',
-                      help="filename prefix of output script containing calls to kdu_compress. default is --output_script_filename_prefix \"%s\"" % output_script_filename_prefix_default )
+                      help="filename prefix of output script containing calls to kdu_compress. default is --output_script_filename_prefix \"%s\"" % output_script_filename_prefix_default, default=output_script_filename_prefix_default)
 parser.add_argument('--path_to_kdu_compress_executable',
-                      help="path to kdu_compress executable. default is --path_to_kdu_compress_executable \"%s\"" % (path_to_kdu_compress_executable_default) ) 
+                      help="path to kdu_compress executable. default is --path_to_kdu_compress_executable \"%s\"" % (path_to_kdu_compress_executable_default), default=path_to_kdu_compress_executable_default) 
 parser.add_argument('--compressed_file_extension',
-                      help="compressed file extension. default is --compressed_file_extension \"%s\"" % (compressed_file_extension_default) )
+                      help="compressed file extension. default is --compressed_file_extension \"%s\"" % (compressed_file_extension_default), default=compressed_file_extension_default )
 parser.add_argument('--is_Clevels_image_size_dependent',
                       help="Use False to use a fixed Clevels value for all images.  The default is True.",
                       type=eval, 
                       choices=[True, False], 
                       default='True')
 parser.add_argument('--encoding_parameter_set',
-                      help="encoding parameter preset value (see python code for preset values to encoding parameter mapping). default is --encoding_parameter_set \"%s\"" % (encoding_parameter_set_default) )
+                      help="encoding parameter preset value (see python code for preset values to encoding parameter mapping). default is --encoding_parameter_set \"%s\"" % (encoding_parameter_set_default),
+                       default=encoding_parameter_set_default)
 parser.add_argument('--smallest_thumbnail_dimension',
-                      help="The smallest thumbnail size that will be requested when decoding. default is --smallest_thumbnail_dimension \"%s\"" % (smallest_thumbnail_dimension_default) )
+                      help="The smallest thumbnail size that will be requested when decoding. default is --smallest_thumbnail_dimension \"%s\"" % (smallest_thumbnail_dimension_default),
+                       default=smallest_thumbnail_dimension_default )
+
+parser.add_argument('--exclude-timing',
+                      help="Include script timing. Note this doesn't work on MacOSX which has a different date command",
+                       action='store_false')
 
 args = parser.parse_args()
 
-if( args.source_directory ) :
-  source_directory = args.source_directory
-  print("processed --source_directory ", source_directory )
-else:
-  source_directory = source_directory_default
-  print("--source_directory command line argument not provided, using default = ", source_directory )
-
-if( args.compressed_directory ) :
-  compressed_directory = args.compressed_directory
-  print("processed --compressed_directory ", compressed_directory )
-else:
-  compressed_directory = compressed_directory_default
-  print("--compressed_directory command line argument not provided, using default = ", compressed_directory )
-
-if( args.logs_directory ) :
-  logs_directory = args.logs_directory
-  print("processed --logs_directory ", logs_directory )
-else:
-  logs_directory = logs_directory_default
-  print("--logs_directory command line argument not provided, using default = ", logs_directory )
-
-if( args.output_script_filename_prefix ) :
-  output_script_filename_prefix = args.output_script_filename_prefix
-  print("processed --output_script_filename_prefix ", output_script_filename_prefix )
-else:
-  output_script_filename_prefix = output_script_filename_prefix_default
-  print("--output_script_filename_prefix command line argument not provided, using default = ", output_script_filename_prefix )
-
-if( args.compressed_file_extension ) :
-  compressed_file_extension = args.compressed_file_extension
-  print("processed --compressed_file_extension ", compressed_file_extension )
-else:
-  compressed_file_extension = compressed_file_extension_default
-  print("--compressed_file_extension command line argument not provided, using default = ", compressed_file_extension )
-
-if( args.encoding_parameter_set ) :
-  encoding_parameter_set = args.encoding_parameter_set
-  print("processed --encoding_parameter_set ", encoding_parameter_set )
-else:
-  encoding_parameter_set = encoding_parameter_set_default
-  print("--encoding_parameter_set command line argument not provided, using default = ", encoding_parameter_set )
-
-if( args.smallest_thumbnail_dimension ) :
-  smallest_thumbnail_dimension = int(args.smallest_thumbnail_dimension)
-  print("processed --smallest_thumbnail_dimension ", smallest_thumbnail_dimension )
-else:
-  smallest_thumbnail_dimension = smallest_thumbnail_dimension_default
-  print("--smallest_thumbnail_dimension command line argument not provided, using default = ", smallest_thumbnail_dimension )
+source_directory = args.source_directory
+compressed_directory = args.compressed_directory
+logs_directory = args.logs_directory
+output_script_filename_prefix = args.output_script_filename_prefix
+compressed_file_extension = args.compressed_file_extension
+encoding_parameter_set = int(args.encoding_parameter_set)
+smallest_thumbnail_dimension = int(args.smallest_thumbnail_dimension)
 
 #codeblock_parameters = ["32,32", "64,64", "32,128"]
 codeblock_parameters = ["64,64"]
@@ -144,12 +109,12 @@ for codeblock_parameter in codeblock_parameters:
     encoding_parameters="Creversible=no -rate 3 Corder=RPCL Cprecincts=\"{256,256}\" ORGgen_plt=yes Cblk=\"{" + codeblock_parameter + "}\""
     test_label_prefix="j2k1_lossy_3bpp_plt_codeblock"
   else:
-    print( "encoding_parameter_set = " + str(encoding_parameter_set) + " is not supported yet, exiting" )
+    print( "encoding_parameter_set = '" + str(encoding_parameter_set) + "' is not supported yet, exiting" )
     exit( -1 )
 
   test_label = test_label_prefix + "_" + codeblock_parameter
 
-  output_script_filename = output_script_filename_prefix_default + "_" + test_label + ".sh"
+  output_script_filename = output_script_filename_prefix + "_" + test_label + ".sh"
 
   # open output file
   output_script_file = open(output_script_filename, 'w')
@@ -163,22 +128,23 @@ for codeblock_parameter in codeblock_parameters:
 
   # set up log file
   output_script_file.write("# setup log files\n")
-  output_script_file.write("date_for_filename=`date +\"%Y-%m-%d-%H-%M-%S_%N\"`\n")
-  output_script_file.write("log_filename=\"" + logs_directory + "/" + test_label + ".${date_for_filename}.log.csv\"\n")
+  if args.exclude_timing:
+    output_script_file.write("date_for_filename=`date +\"%Y-%m-%d-%H-%M-%S_%N\"`\n")
+    output_script_file.write("log_filename=\"" + logs_directory + "/" + test_label + ".${date_for_filename}.log.csv\"\n")
  
-  # write header to log file
-  output_script_file.write("# create log header\n")
-  output_script_file.write("printf \"output_directory,encode_time_in_seconds,compressed_size_in_bytes\n\" > $log_filename\n")
+    # write header to log file
+    output_script_file.write("# create log header\n")
+    output_script_file.write("printf \"output_directory,encode_time_in_seconds,compressed_size_in_bytes\n\" > $log_filename\n")
 
-  # add output directory to log file  
-  output_script_file.write("# add output directory to log file\n")
-  output_script_file.write("printf \"\\\"" + output_directory + "\\\",\" >> $log_filename\n")
+    # add output directory to log file  
+    output_script_file.write("# add output directory to log file\n")
+    output_script_file.write("printf \"\\\"" + output_directory + "\\\",\" >> $log_filename\n")
 
-  # start timer
-  output_script_file.write("# start timer\n")
-  output_script_file.write("job_start_time=`date +%s%N`\n")
+    # start timer
+    output_script_file.write("# start timer\n")
+    output_script_file.write("job_start_time=`date +%s%N`\n")
 
-  output_script_file.write("# do compression of input files\n")
+    output_script_file.write("# do compression of input files\n")
   for source_file in list_of_source_files:
       
     if source_file.endswith(input_file_extension):
@@ -216,42 +182,43 @@ for codeblock_parameter in codeblock_parameters:
         Clevels_parameter = 6
 
       kdu_compress_complete_parameters_without_i_and_o = encoding_parameters + " Clevels=" + str(Clevels_parameter)
-      kdu_compress_command = path_to_kdu_compress_executable_default + " -i " + input_filename + " -o " + output_filename + " " + kdu_compress_complete_parameters_without_i_and_o
+      kdu_compress_command = args.path_to_kdu_compress_executable + " -i " + input_filename + " -o " + output_filename + " " + kdu_compress_complete_parameters_without_i_and_o
       # create kdu_compress command and include the command itself in a com marker
       kdu_compress_command_for_com_marker = "\"" + "kdu_compress " +  kdu_compress_complete_parameters_without_i_and_o.replace("\"", "\\\"" ) + "\""
       output_script_file.write(kdu_compress_command + " -com " + kdu_compress_command_for_com_marker + "\n")
 
-  # stop timer
-  output_script_file.write("# stop timer\n")
-  output_script_file.write("job_end_time=`date +%s%N`\n")
+  if args.exclude_timing:
+    # stop timer
+    output_script_file.write("# stop timer\n")
+    output_script_file.write("job_end_time=`date +%s%N`\n")
 
-  # compute timing stats for the encoding
-  output_script_file.write("# compute timing stats for the encoding\n")
-  output_script_file.write("let duration_nanoseconds=$job_end_time-$job_start_time\n")
-  output_script_file.write("number_of_nanoseconds_in_one_second=1000000000\n")
-  output_script_file.write("duration_total_seconds=$(echo \"scale=5;$duration_nanoseconds/$number_of_nanoseconds_in_one_second\" | bc)\n")
+    # compute timing stats for the encoding
+    output_script_file.write("# compute timing stats for the encoding\n")
+    output_script_file.write("let duration_nanoseconds=$job_end_time-$job_start_time\n")
+    output_script_file.write("number_of_nanoseconds_in_one_second=1000000000\n")
+    output_script_file.write("duration_total_seconds=$(echo \"scale=5;$duration_nanoseconds/$number_of_nanoseconds_in_one_second\" | bc)\n")
 
-  # print timing of encoding to screen
-  output_script_file.write("# print timing of encoding to screen\n")
-  output_script_file.write("printf \"duration_total_seconds=%s\n\" $duration_total_seconds\n")
+    # print timing of encoding to screen
+    output_script_file.write("# print timing of encoding to screen\n")
+    output_script_file.write("printf \"duration_total_seconds=%s\n\" $duration_total_seconds\n")
 
-  # print timing of encoding to file
-  output_script_file.write("# print timing of encoding to file\n")
-  output_script_file.write("printf \"%s,\" $duration_total_seconds >> $log_filename\n")
+    # print timing of encoding to file
+    output_script_file.write("# print timing of encoding to file\n")
+    output_script_file.write("printf \"%s,\" $duration_total_seconds >> $log_filename\n")
 
-  # get number of bytes for the folder of compressed files
-  output_script_file.write("# get number of bytes for the folder of compressed files\n")
-  output_script_file.write("bytes_in_compressed_folder_with_dot_dir=`du -sb " + output_directory + " | awk '{print $1}'`\n")
-  output_script_file.write("# subtract 4096 for the '.' dir\n")
-  output_script_file.write("let bytes_in_compressed_folder=$bytes_in_compressed_folder_with_dot_dir-4096\n")
+    # get number of bytes for the folder of compressed files
+    output_script_file.write("# get number of bytes for the folder of compressed files\n")
+    output_script_file.write("bytes_in_compressed_folder_with_dot_dir=`du -sb " + output_directory + " | awk '{print $1}'`\n")
+    output_script_file.write("# subtract 4096 for the '.' dir\n")
+    output_script_file.write("let bytes_in_compressed_folder=$bytes_in_compressed_folder_with_dot_dir-4096\n")
 
-  # print results to screen
-  output_script_file.write("# print results to screen\n")
-  output_script_file.write("printf \"bytes_in_compressed_folder=%s\n\" $bytes_in_compressed_folder\n")
+    # print results to screen
+    output_script_file.write("# print results to screen\n")
+    output_script_file.write("printf \"bytes_in_compressed_folder=%s\n\" $bytes_in_compressed_folder\n")
 
-  # add timing info to log
-  output_script_file.write("# add timing info to log\n")
-  output_script_file.write("printf \"%s\n\" $bytes_in_compressed_folder >> $log_filename\n")
+    # add timing info to log
+    output_script_file.write("# add timing info to log\n")
+    output_script_file.write("printf \"%s\n\" $bytes_in_compressed_folder >> $log_filename\n")
 
   # close files before exiting
   output_script_file.close()
