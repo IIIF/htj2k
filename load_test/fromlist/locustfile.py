b320fef7ede7880af76eff409fbbd6c8a3c71cd8
Glen Robson <glen.robson@gmail.com>
2023-05-24 02:32:35 +0100

    Updating jp2 extensions

--- load_test/fromlist/locustfile.py
+++ load_test/fromlist/locustfile.py
@@ -2,29 +2,69 @@
 from locust import HttpUser, task, events
 
 locations = {
-    'ptiff-lossy': {
+    'ptiff-lossy-round1': {
         'loc': 'ptiff/lossy',
         'ext': 'tif'
     },
-    'ptiff-lossless': {
+    'ptiff-lossless-round1': {
         'loc': 'ptiff/lossless',
         'ext': 'tif'
     },
-    'jp2-lossy': {
+    'jp2-lossy-round1': {
         'loc': 'jp2/lossy',
         'ext': 'jp2'
     },
-    'jp2-lossless': {
+    'jp2-lossless-round1': {
         'loc': 'jp2/lossless',
         'ext': 'jp2'
     },
-    'htj2k-lossy': {
+    'htj2k-lossy-round1': {
         'loc': 'htj2k/lossy',
         'ext': 'htj2k.jp2'
     },
-    'htj2k-lossless': {
+    'htj2k-lossless-round1': {
         'loc': 'htj2k/lossless',
         'ext': 'htj2k.jp2'
+    },
+    'htj2k-bodelian-lossless': {
+        'loc': 'params/htj2k_digital_bodelian_lossless_codeblock_64,64',
+        'ext': 'jph'
+    },
+    'htj2k_bodelian_lossy': {
+        'loc': 'params/htj2k_digital_bodelian_lossy_codeblock_64,64',
+        'ext': 'jph'
+    },
+    'htj2k_lossless': {
+        'loc': 'params/htj2k_lossless_codeblock_64,64',
+        'ext': 'jph'
+    },
+    'htj2k_lossless_plt': {
+        'loc': 'params/htj2k_lossless_plt_codeblock_64,64',
+        'ext': 'jph'
+    },
+    'htj2k_lossy_3bpp_plt': {
+        'loc': 'params/htj2k_lossy_3bpp_plt_codeblock_64,64',
+        'ext': 'jph'
+    },
+    'htj2k_lossy_Qfactor_90_plt': {
+        'loc': 'params/htj2k_lossy_Qfactor_90_plt_codeblock_64,64',
+        'ext': 'jph'
+    },
+    'j2k_bodelian_lossless': {
+        'loc': 'params/j2k1_digital_bodelian_lossless_codeblock_64,64',
+        'ext': 'jp2'
+    },
+    'j2k1_bodelian_lossy': {
+        'loc': 'params/j2k1_digital_bodelian_lossy_codeblock_64,64',
+        'ext': 'jp2'
+    },
+    'j2k1_lossless_plt': {
+        'loc': 'params/j2k1_lossless_plt_codeblock_64,64',
+        'ext': 'jp2'
+    },
+    'j2k1_lossy_3bpp_plt': {
+        'loc': 'params/j2k1_lossy_3bpp_plt_codeblock_64,64',
+        'ext': 'jp2'
     }
 }
 
@@ -73,5 +113,6 @@ class IIIFURLTester(HttpUser):
 
                 self.client.get(url,name=name) 
 
-        self.environment.runner.stop()        
-
+        print ('finished tests')
+        self.environment.runner.quit()        
+        print ('Quiting')
